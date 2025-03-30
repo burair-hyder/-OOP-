01437cbd4b5949baf3b44b018f11c7b0bd48e770
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-03-31 02:19:35 +0500

    Update question6.cpp

--- OOP-LAB/Lab Task 05/question6.cpp
+++ OOP-LAB/Lab Task 05/question6.cpp
@@ -1,1 +1,58 @@
-s
+#include <iostream>
+
+using namespace std;
+
+class GraphicsEngine {
+public:
+    GraphicsEngine() {
+        cout << "Initializing Graphics Engine...\n";
+    }
+    void render() {
+        cout << "Displaying visuals on screen...\n";
+    }
+};
+
+class InputHandler {
+public:
+    InputHandler() {
+        cout << "Setting up Input Handler...\n";
+    }
+    void processInput() {
+        cout << "Capturing and interpreting user input...\n";
+    }
+};
+
+class PhysicsEngine {
+public:
+    PhysicsEngine() {
+        cout << "Starting Physics Engine...\n";
+    }
+    void updatePhysics() {
+        cout << "Simulating physics calculations...\n";
+    }
+};
+
+class GameEngine {
+private:
+    GraphicsEngine graphics;
+    InputHandler input;
+    PhysicsEngine physics;
+
+public:
+    GameEngine() {
+        cout << "Booting up Game Engine...\n";
+    }
+    void runGameLoop() {
+        cout << "Beginning Main Game Loop...\n";
+        input.processInput();
+        physics.updatePhysics();
+        graphics.render();
+        cout << "Ending Game Cycle...\n";
+    }
+};
+
+int main() {
+    GameEngine game;
+    game.runGameLoop();
+    return 0;
+}
