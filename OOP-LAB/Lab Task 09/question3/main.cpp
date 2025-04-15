7ed71aed7573245584af1005864f18321cd4ed1c
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-04-15 15:01:57 +0500

    Update main.cpp

--- OOP-LAB/Lab Task 09/question3/main.cpp
+++ OOP-LAB/Lab Task 09/question3/main.cpp
@@ -1,12 +1,15 @@
-#include "Book.h"
 #include <iostream>
-using namespace std;
+#include "Book.h"
 
-
-int main(){
-	Book mybook("HOW to LIVE LIFE","1231231231","Burair");
-	cout <<mybook.getTitle()<<endl;
-	cout <<mybook.getAuthor()<<endl;
-	cout <<mybook.getISBN()<<endl;
-	return 0;
+int main() {
+    Book mybook("MY friend","123123","burair hyder");
+        Book mybook1("MY friend","123123","burair hyder");
+        Library l1;
+        l1.addbook();
+        l1.addbook();
+        l1.searchbook();
+    cout <<mybook.getAuthor()<<endl;
+    cout <<mybook.getTitle()<<endl;
+    cout <<mybook.getISBN()<<endl;
+    return 0;
 }
