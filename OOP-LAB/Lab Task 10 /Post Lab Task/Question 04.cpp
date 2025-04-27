c309caf8cb1f97a2ad98b958cc3d83315d74b2de
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-04-27 19:56:59 +0500

    Update Question 04.cpp

--- OOP-LAB/Lab Task 10 /Post Lab Task/Question 04.cpp
+++ OOP-LAB/Lab Task 10 /Post Lab Task/Question 04.cpp
@@ -1,1 +1,37 @@
-q4
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int  main(){
+	char buffer[9] ={0};
+	ofstream file;
+	file.open("info.txt",ios::out);
+	file <<"C++ is a powerful programming language.\n";
+	file.close();
+	fstream file1;
+	file1.open("info.txt",ios::in|ios::out);
+	streampos pos = file1.tellg();
+	cout <<"get Pointer position : "<<pos<<endl;
+	pos = file1.tellp();
+	cout <<"Put Pointer Position : "<<pos <<endl;
+	file1.seekg(9);
+	file1.read(buffer,8);
+	cout <<buffer<<endl;
+	file1.seekp(9);
+	file1.write("Dynamicc",8);
+	
+	pos = file1.tellg();
+	cout <<"Updated Get Pointer Position : "<<pos<<endl;
+	pos = file1.tellg();
+	cout <<"Updated Put Pointer Position: "<<pos <<endl;
+	cout <<endl;
+	cout <<"updated File Content:" <<endl;
+	string line;
+	cout <<endl;
+	file1.seekp(0,ios::beg);
+	while (getline(file1,line)){
+		cout <<line<<endl;
+	}
+	file1.close();
+	
+}
